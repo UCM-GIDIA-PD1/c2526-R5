5bb8e3e1ef63a7ff4032816f08f8e01081fb2bc6
IvanGavaaaa <ivagar13@ucm.es>
2026-02-27 12:51:16 +0100

    Ingest de Clima

--- src/pipelines/run_extraccion.py
+++ src/pipelines/run_extraccion.py
@@ -10,7 +10,7 @@ import argparse
 import sys
 from typing import Callable, Dict, List
 
-#from src.clima.ingest import ingest_clima
+from clima.ingest_clima_historico import ingest_clima_historico
 #from src.eventos.ingest import ingest_eventos
 from src.gtfs_historico.ingest import process_and_store_gtfs_range as ingest_gtfs_historico
 #from src.alertas_oficiales_tiempo_real.ingest import ingest_alertas
@@ -19,7 +19,7 @@ from src.gtfs_historico.ingest import process_and_store_gtfs_range as ingest_gtf
 IngestFn = Callable[[str, str], None]
 
 REGISTRY: Dict[str, IngestFn] = {
-    #"clima": ingest_clima,
+    "clima_ historico": ingest_clima_historico,
     #"eventos": ingest_eventos,
     "gtfs_historico": ingest_gtfs_historico,
     #"tiempo_real_metro": ingest_tiempo_real,
